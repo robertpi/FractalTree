8c052a93c4ecd08df13e9ef2a1345f709800adfd
Robert Pickering <robert@sqreen.io>
2020-12-26 12:10:44 +0100

    Broken tree with interesting debug code

--- FractalTrees/Tree.fs
+++ FractalTrees/Tree.fs
@@ -13,38 +13,44 @@ let drawTree file =
     g.FillRectangle(new SolidBrush(Color.White), 0, 0, size, size)
     g.DrawRectangle(new Pen(Color.Black, 1.f), 0, 0, size - 1, size - 1)
 
-    let drawLeaf() = ()
-    let drawTrunk (startX, startY) direction length =
-        let x = length * Math.Sin(direction)
-        let y = length * Math.Cos(direction)
-        let nextX, nextY = startX + x, startY + y
-        let width = length / 15. 
-        let widthX = width * Math.Cos(direction)
-        let widthY = width * Math.Sin(direction)
-
-        let points = 
-            [| new Point(int (startX - widthX), int (startY - widthY)); 
-               new Point(int (nextX - widthX), int (nextY - widthY)); 
-               new Point(int (nextX + widthX), int (nextY + widthY)); 
-               new Point(int (startX + widthX), int (startY + widthY)) |]
-        let pen = new Pen(Color.Black, float32 length / 15.f)
-        g.FillClosedCurve(new SolidBrush(Color.Black), points)
-        (nextX, nextY)
-
     let largeAngle = Math.PI / 2.4
     let smallAngle = 0.2
-    let factor n = 0.6 
+    let factor = 0.6 
 
-    let rec innerDrawTree n location direction  length =
+    let rec innerDrawTree n startX startY direction length acc =
         if n > 0 then
-            let location = drawTrunk location direction length
-            let op1, op2 = if n % 2 = 0 then (-), (+) else (+), (-)  
-            innerDrawTree (n - 1) location (op1 direction largeAngle) (length * factor n)
-            innerDrawTree (n - 1) location (op2 direction smallAngle) (length * factor n)
-        else
-            drawLeaf()
+            let x = length * Math.Sin(direction)
+            let y = length * Math.Cos(direction)
+            let nextX, nextY = startX + x, startY + y
+            let width = length / 15. 
+            let widthX = width * Math.Cos(direction)
+            let widthY = width * Math.Sin(direction)
 
-    innerDrawTree 10 ((float size) / 2., (float size) - 50.) (Math.PI * 0.98) 300.
+            let acc = new Point(int (startX - widthX), int (startY - widthY)) :: acc
+
+            let ang1, ang2 = if n % 2 = 0 then smallAngle, largeAngle else largeAngle, smallAngle  
+            let acc = innerDrawTree (n - 1) nextX nextY (direction - ang1) (length * factor) acc
+            let acc = innerDrawTree (n - 1) nextX nextY (direction + ang2) (length * factor) acc
+            
+            new Point(int (startX + widthX), int (startY + widthY)) :: acc
+        else
+            acc
+
+    let startAngle = Math.PI // Math.PI * 0.98
+    let points = 
+        innerDrawTree 3 ((float size) / 2.) ((float size) - 50.) startAngle 300. []
+        |> List.toArray
+
+    printfn "%A" points
+
+    g.FillClosedCurve(new SolidBrush(Color.Black), points)
+
+
+    let drawFont = new Font(FontFamily.GenericMonospace, 16.f);
+    let drawBrush = new SolidBrush(Color.Red);
+
+    points
+    |> Seq.iteri (fun i p -> g.DrawString(string i, drawFont, drawBrush, float32 p.X, float32 p.Y))
 
     img.Save(file)
 
