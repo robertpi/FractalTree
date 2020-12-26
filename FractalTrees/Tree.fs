cd4d193abe53e7601b0f8708407bf1d5c8b491a6
Robert Pickering <robert@sqreen.io>
2020-12-26 09:59:31 +0100

    Alternating tree

--- FractalTrees/Tree.fs
+++ FractalTrees/Tree.fs
@@ -8,9 +8,9 @@ let size = 1000
 let drawTree file = 
     let rnd = new Random()
     let img = new Bitmap(size, size)
+    let pen = new Pen(Color.Black, 1.f)
     use g = Graphics.FromImage(img)
     g.SmoothingMode <- SmoothingMode.HighQuality;
-    let pen = new Pen(Color.Black, 1.f)
     g.FillRectangle(new SolidBrush(Color.White), 0, 0, size, size)
     g.DrawRectangle(new Pen(Color.Black, 1.f), 0, 0, size - 1, size - 1)
 
@@ -22,18 +22,20 @@ let drawTree file =
         g.DrawLine(pen, int startX, int startY, int nextX, int nextY)
         (nextX, nextY)
 
-    let randomAngle() = rnd.NextDouble() * 0.6
+    let largeAngle = Math.PI / 2.4
+    let smallAngle = 0.2
     let factor n = 0.6 
 
     let rec innerDrawTree n location direction  length =
         if n > 0 then
             let location = drawTrunk location direction length
-            innerDrawTree (n - 1) location (direction + randomAngle()) (length * factor n)
-            innerDrawTree (n - 1) location (direction - randomAngle()) (length * factor n)
+            let op1, op2 = if n % 2 = 0 then (-), (+) else (+), (-)  
+            innerDrawTree (n - 1) location (op1 direction largeAngle) (length * factor n)
+            innerDrawTree (n - 1) location (op2 direction smallAngle) (length * factor n)
         else
             drawLeaf()
 
-    innerDrawTree 10 ((float size) / 2., (float size) - 50.) Math.PI 300.
+    innerDrawTree 10 ((float size) / 2., (float size) - 50.) (Math.PI * 0.98) 300.
 
     img.Save(file)
 
